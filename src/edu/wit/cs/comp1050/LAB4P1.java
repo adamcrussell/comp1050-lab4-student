6e94d68b2ae09d32d0eefec8867285d90802fbb4
Jones Yu <yuj6@wit.edu>
2018-10-08 18:27:01 -0400

    init commit

---
+++ src/edu/wit/cs/comp1050/LAB4P1.java
@@ -0,0 +1,119 @@
+/*********************************************************************
+*  LAB 4 - Problem 1
+*
+*  YOUR NAME HERE
+*  COMP1050
+*  XX/XX/2018  (UPDATE THE DATE) 
+*
+**********************************************************************
+*  Problem Description (PLEASE UPDATE THE DESCRIPTION)
+*  
+*  Write a program that reads two numbers from the user and
+*  prints out the sum of those two numbers.
+*  
+***********************************************************************
+*  Analysis (PLEASE UPDATE THE DESCRIPTION)
+*  
+*  For this problem, I try to have the user's input and sum the values. 
+*  The steps to solve the problem are:
+*   
+*    Step 1: Declare a scanner object, say input
+*    Step 2: Use scanner object to get two integer values
+*    Step 3: ...
+*    
+**********************************************************************/
+
+package edu.wit.cs.comp1050;
+
+public class LAB4P1 {
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+}
+
+class MyInteger {
+	
+	// TODO: write your code here
+	
+	public int getValue() {
+		// TODO: write your code here
+		return 0;
+	}
+
+	public MyInteger(int value) {
+		// TODO: write your code here
+	}
+
+	public boolean isPrime() {
+		// TODO: write your code here
+		return false;
+	}
+
+	public static boolean isPrime(int num) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public static boolean isPrime(MyInteger o) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public boolean isEven() {
+		// TODO: write your code here
+		return false;
+	}
+
+	public boolean isOdd() {
+		// TODO: write your code here
+		return false;
+	}
+
+	public static boolean isEven(int n) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public static boolean isOdd(int n) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public static boolean isEven(MyInteger n) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public boolean equals(int anotherNum) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public boolean equals(MyInteger o) {
+		// TODO: write your code here
+		return false;
+	}
+
+	public static int parseInt(char[] numbers) {
+		// numbers consists of digit characters.
+		// For example, if numbers is {'1', '2', '5'}, the return value
+		// should be 125. Please note that
+		// numbers[0] is '1'
+		// numbers[1] is '2'
+		// numbers[2] is '5'
+
+		// TODO: write your code here
+		
+		return 0;
+	}
+
+	public static int parseInt(String s) {
+		// s consists of digit characters.
+		// For example, if s is "125", the return value
+		// should be 125.
+		
+		// TODO: write your code here
+		
+		return 0;
+	}
+}
---
+++ src/edu/wit/cs/comp1050/LAB4P2.java
@@ -0,0 +1,59 @@
+/*********************************************************************
+*  LAB 4 - Problem 1
+*
+*  YOUR NAME HERE
+*  COMP1050
+*  XX/XX/2018  (UPDATE THE DATE) 
+*
+**********************************************************************
+*  Problem Description (PLEASE UPDATE THE DESCRIPTION)
+*  
+*  Write a program that reads two numbers from the user and
+*  prints out the sum of those two numbers.
+*  
+***********************************************************************
+*  Analysis (PLEASE UPDATE THE DESCRIPTION)
+*  
+*  For this problem, I try to have the user's input and sum the values. 
+*  The steps to solve the problem are:
+*   
+*    Step 1: Declare a scanner object, say input
+*    Step 2: Use scanner object to get two integer values
+*    Step 3: ...
+*    
+**********************************************************************/
+
+package edu.wit.cs.comp1050;
+
+public class LAB4P2 {
+	public static void main(String[] args){
+		// TODO: write your code here
+	}
+}
+
+class RoomPeople {
+    
+	// TODO: write your code here
+    
+    public static int getTotal(){
+    	// TODO: write your code here
+    	return 0;
+    }
+    
+    public RoomPeople() {
+    	// TODO: write your code here
+    }
+    
+    public void addOneToRoom(){
+    	// TODO: write your code here
+    }
+    
+    public void removeOneFromRoom(){
+    	// TODO: write your code here
+    }
+    
+    public int getNumber(){
+    	// TODO: write your code here
+        return 0;
+    }
+}   
